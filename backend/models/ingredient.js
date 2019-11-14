bf85fa909d07f6fed63d4d710d8774f9d868a98d
yejikk <dpwl7484@gmail.com>
2019-11-14 16:09:08 +0900

    file delete

--- backend/models/ingredient.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ingredientSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  picture: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Ingedient", ingredientSchema);
--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  userid: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  ingredients: Array
-});
-
-module.exports = mongoose.model("User", userSchema);
